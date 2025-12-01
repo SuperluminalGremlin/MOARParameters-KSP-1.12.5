a2fc1c2c7688bb0a093fa56b3815492857dd29ef
SuperluminalGremlin <willstanley444@gmail.com>
2025-12-01 11:40:30 +1100

    Initial Source Code

---
+++ Downrange Param/Downrange Param/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+[KSPAddon(KSPAddon.Startup.Instantly, true)]
+public class DebugLoader : MonoBehaviour
+{
+    void Start()
+    {
+        try
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            asm.GetTypes();
+        }
+        catch (ReflectionTypeLoadException ex)
+        {
+            Debug.LogError("[Downrange] LoaderExceptions:");
+            foreach (var e in ex.LoaderExceptions)
+            {
+                Debug.LogError("[Downrange] " + e.Message);
+            }
+        }
+    }
+}
